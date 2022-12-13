58adb94e183862ed4fcf6626a92b326722e59d8b
Trina <trina.rundgren@telus.com>
2022-12-13 14:35:57 -0800

    renderTweet logic complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const createTweetElement = function(data) {
+  return (`<article class="tweet">
+  <header>
+    <div class="user">
+      <img
+        src="${data.user.avatars}"
+        alt="">
+      <p>${data.user.name}</p>
+    </div>
+    <h4>${data.user.handle}</h4>
+  </header>
+  <p>${data.content.text}</p>
+  <footer>
+    <span>${data.created_at}</span>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+</article>`);
+};
+
+const renderTweet = function(data) {
+  for (let tweet of data) {
+    let tweetTemp = createTweetElement(tweet);
+    $('#tweets-container').append(tweetTemp);
+  }
+}
+
+$(document).ready(function() {
+  renderTweet(data);
+
+});
