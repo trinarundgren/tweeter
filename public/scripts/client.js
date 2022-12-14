fdd11376089443680ea61f9606d752684772e6ab
Trina <trina.rundgren@telus.com>
2022-12-13 19:50:51 -0800

    add escape function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,23 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const escape = function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 
-const createTweetElement = function(data) {
-  let $tweet = $(`
+$(document).ready(function () {
+
+  //turns tweet objects into HTML formatted tweet articles
+  const createTweetElement = function(data) {
+    let $tweet = $(`
   <article class="tweet">
   <header>
     <div class="user">
       <img
-        src="${data.user.avatars}"
+      src="${escape(data.user.avatars)}"
         alt="">
-      <p>${data.user.name}</p>
+        <p>${escape(data.user.name)}</p>
     </div>
-    <h4>${data.user.handle}</h4>
+    <h4>${escape(data.user.handle)}</h4>
   </header>
-  <p>${data.content.text}</p>
+  <p>${escape(data.content.text)}</p>
   <footer>
-    <span>${data.created_at}</span>
     <div>
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -29,59 +36,45 @@ const createTweetElement = function(data) {
   </footer>
 </article>
 `);
-return $tweet;
-};
+    $('#tweet-container').prepend($tweet);
+  };
 
-const renderTweet = function(data) {
-  for (let tweet of data) {
-    $('#tweets-container').append(createTweetElement(tweet));
-  }
-};
-
-const loadTweets = function() {
-  $.ajax('/tweets', { method: 'GET' })
-    .then((tweets) => {
-      renderTweet(tweets.reverse())
-    })
-    .catch((err) => {
-      console.log("There was an ERROR ", err)
-    })
-};
-
-//loads tweets on page load
-loadTweets()
-
-$(document).ready(function() {
-  console.log('doc is ready')
+  const renderTweet = function(data) {
+    
+    for (let tweet of data) {
+      createTweetElement(tweet);
+    }
+  };
 
   $('form.tweetSubmit').on('submit', function(event) {
 
     event.preventDefault();
-    
-    if (!$('.tweet-text').val()) {
+
+    if (!$('textarea').val().length) {
       return alert('You cannot post an empty tweet')
     }
-    if ($('.tweet-text').val().length > 140) {
+    if ($('textarea').val().length > 140) {
       return alert("Your tweet exceeds the maximum characters")
     }
-
+//tweet submission to database
     $.ajax('/tweets', {
       method: 'POST',
       data: $(this).serialize()
-    })
-      .then(function(tweet) {
-  
-        $('.tweet-text').val('');
-      })
-      .then(() => {
-        loadTweets();
-      })
-      .catch((err) => {
-        console.log('There was an error', err)
-      })
+    }).then(loadTweets)
+    $('textarea').val("")
 
   });
 
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(renderTweet)
+
+      .catch((err) => {
+        console.log("There was an ERROR ", err)
+      })
+  };
+
+  loadTweets()
 
 
 });
