a7de8876d56c58e1729effd23cd776105af8265b
Trina <trina.rundgren@telus.com>
2022-12-13 16:25:15 -0800

    load tweets complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 const createTweetElement = function(data) {
-  let $tweet = $(`<article class="tweet">
+  let $tweet = $(`
+  <article class="tweet">
   <header>
     <div class="user">
       <img
@@ -51,7 +27,8 @@ const createTweetElement = function(data) {
       <i class="fas fa-heart"></i>
     </div>
   </footer>
-</article>`);
+</article>
+`);
 return $tweet;
 };
 
@@ -64,8 +41,7 @@ const renderTweet = function(data) {
 const loadTweets = function() {
   $.ajax('/tweets', { method: 'GET' })
     .then((tweets) => {
-      console.log(tweets)
-      return tweets;
+      renderTweet(tweets)
     })
     .catch((err) => {
       console.log("There was an ERROR ", err)
@@ -85,14 +61,14 @@ $(document).ready(function() {
       data: $(this).serialize()
     })
       .then(function(tweet) {
-        console.log('Tweet has successfully been sent to database');
+  
         $('.tweet-text').val('')
       })
       .catch((err) => {
         console.log('There was an error', err)
       })
   });
-  renderTweet(data);
 
-  
+
+
 });
